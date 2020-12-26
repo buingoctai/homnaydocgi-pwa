cd0be58ddb72d75a31ce5e977c4c8a83af40c434
buingoctai <buingoctai994@gmail.com>
2020-12-26 16:46:43 +0700

    fix css

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -15,9 +15,40 @@ const Article = () => {
   const [readedList, setReadedList] = useState({});
   console.log(' totalRecord, data', totalRecord, data);
 
+  useEffect(() => {
+    let deferredPrompt;
+    const addBtn = document.getElementById('btn-add');
+    //  addBtn.style.display = 'none';
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      console.log('beforeinstallprompt');
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI to notify the user they can add to home screen
+      addBtn.style.display = 'block';
+
+      addBtn.addEventListener('click', (e) => {
+        // hide our user interface that shows our A2HS button
+        addBtn.style.display = 'none';
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+        });
+      });
+    });
+  }, []);
   return totalRecord ? (
     <div style={{ width: '100%', height: '100%' }}>
-      <button id="btn-add" style={{ width: '100%', height: '35px', fontSize: '25px' }}>
+      <button id="btn-add" style={{ width: '100%', height: '60px', fontSize: '45px' }}>
         Add To Home Screen
       </button>
       <div className="article__container" style={{ width: '100%', height: '100%' }}>
@@ -38,7 +69,7 @@ const Article = () => {
                   >
                     {({ measure }) => (
                       <div
-                        style={{ ...style, margin: '0px 8px' }}
+                        style={{ ...style, padding: '0px 10px' }}
                         className="article__wrap"
                         data-id={index}
                       >
