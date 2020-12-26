c3af389127e8dd5cc559e5ab9f3373d03cf5a41f
buingoctai <buingoctai994@gmail.com>
2020-12-26 16:29:33 +0700

    update sw

--- public/sw.js
+++ public/sw.js
@@ -7,19 +7,19 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
-  // console.log('url', event.request.url);
-  // console.log('url', event.request);
-  // event.respondWith(
-  //   caches.open('content').then(function (cache) {
-  //     return cache.match(event.request).then(function (response) {
-  //       return (
-  //         response ||
-  //         fetch(event.request).then(function (response) {
-  //           cache.put(event.request, response.clone());
-  //           return response;
-  //         })
-  //       );
-  //     });
-  //   })
-  // );
+  console.log('url', event.request.url);
+  console.log('request', event.request);
+  event.respondWith(
+    caches.open('content').then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        return (
+          response ||
+          fetch(event.request).then(function (response) {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+        );
+      });
+    })
+  );
 });
