58d997b79d07ea0195f3a34af025158a96df7e59
LAP13002\LAP13002-local <buingoctai994@gmail.com>
2021-02-24 21:59:27 +0700

    break para

--- src/pages/Article/components/content/index.js
+++ src/pages/Article/components/content/index.js
@@ -33,11 +33,22 @@ const Content = ({ index, post, listRef, heightStore, readedList, setReadedList 
       delete readedList[Id];
     }
   }, []);
+  const breakContent = (content) => {
+    const contentList  = content ? content.split('\n') : [];
+    const contentHtml = contentList.map(paragraph=>(
+      <>
+        {paragraph}
+        <br />
+      </>
+    ));
+
+    return contentHtml;
+  }
   return (
     <div className="full-content">
       <h1 className="title">{capitalize(Title.toLowerCase())}</h1>
       <p className="content">
-        {readedList[Id] ? detailPost.Content : Brief}
+        {readedList[Id] ? breakContent(detailPost.Content) : Brief}
         {!readedList[Id] && (
           <a href="#" onClick={() => onReadMore()} className="button-more">
             ...Xem thêm
